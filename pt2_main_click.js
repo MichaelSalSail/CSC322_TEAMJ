c0a3ba9d023e64e39bd561e1433fa3306e3b7187
Michael Salamon <msalamo000@citymail.cuny.edu>
2021-03-30 12:51:02 -0400

    pt2 Clicking Button #3

--- pt2_main_click.js
+++ pt2_main_click.js
@@ -2,11 +2,10 @@ const{app,BrowserWindow}=require('electron')
 const url =require('url')
 const path =require('path')
 
-let win  
+let win
 
 function createWindow()
-{ 
-   // Module 1 - BrowserWindow
+{
    win =new BrowserWindow({width:800, height:600,
                               webPreferences:
                               {
@@ -14,19 +13,10 @@ function createWindow()
                                  contextIsolation: false
                               }
                            }) 
-
-   /*win.loadFile('index.html')
-   win.on('closed', function () 
-   {
-         win = null
-   })*/                        
-
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname,'index.html'), 
-      protocol:'file:', 
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname,'pt2_index_click.html'),
+      protocol:'file:',
       slashes:true}))
-}  
+}
 
-// Module 2 - app
 app.on('ready', createWindow)
-
