0b8428c34941949c313217dc4ea7af74bef6f8b8
31744748+DD155@users.noreply.github.com <31744748+DD155@users.noreply.github.com>
2021-05-12 13:34:33 -0400

    same

--- Javascript Website/Delivery Subsystem/delivery.js
+++ Javascript Website/Delivery Subsystem/delivery.js
@@ -334,49 +334,4 @@ function start() {
     }
     initializeNavigation();
     setupLoginButton();
-
-    
 }
-
-
-/*
-function toggleTabs() {
-    let availableTab = document.getElementById('available-tab');
-    let availableDisplay = document.getElementById('available-display');
-    let pendingTab = document.getElementById('pending-tab');
-    let pendingDisplay = document.getElementById('pending-display');
-    let approvedTab = document.getElementById('approved-tab');
-    let approvedDisplay = document.getElementById('approved-display');
-
-    let isAvailableSelected = availableTab.disabled;
-    let isPendingSelected = pendingTab.disabled;
-    let isApprovedSelected = approvedTab.disabled;
-
-    if (isAvailableSelected & isPendingSelected & !isApprovedSelected) 
-    {
-        availableTab.disabled = false;
-        pendingTab.disabled = true;
-        approvedTab.disabled = true;
-        availableDisplay.style.display = 'block';
-        pendingDisplay.style.display = 'none';
-        approvedDisplay.style.display = 'none';
-    } 
-    else if (!isAvailableSelected & isPendingSelected & isApprovedSelected)
-    {
-        availableTab.disabled = true;
-        pendingTab.disabled = false;
-        approvedTab.disabled = true;
-        availableDisplay.style.display = 'none';
-        pendingDisplay.style.display = 'block';
-        approvedDisplay.style.display = 'none';
-    }
-    else if (isAvailableSelected & !isPendingSelected & isApprovedSelected)
-    {
-        availableTab.disabled = true;
-        pendingTab.disabled = true;
-        approvedTab.disabled = false;
-        availableDisplay.style.display = 'none';
-        pendingDisplay.style.display = 'none';
-        approvedDisplay.style.display = 'block';
-    }
-}*/
